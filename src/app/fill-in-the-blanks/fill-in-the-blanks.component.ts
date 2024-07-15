9077e74c703fc0ac1bc01adac8f65277e42ddeae
kzafirov <kiril.zafir@gmail.com>
2024-07-15 20:25:29 +0200

    adjust config file

--- src/app/fill-in-the-blanks/fill-in-the-blanks.component.ts
+++ src/app/fill-in-the-blanks/fill-in-the-blanks.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { widgetDisplayOptions } from '../config/config';
 import { DynamicFormConfig } from '../dynamic-forms/dynamic-forms.model';
-import { tap } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'fill-in-the-blanks',
@@ -16,19 +15,12 @@ export class FillInTheBlanksComponent {
   configDisplayOptions: DynamicFormConfig = {
     description: 'FIll In The Blanks',
     controls: widgetDisplayOptions.formData as any
-  } ;
- 
+  };
+
   @Output() buildForm = new EventEmitter<DynamicFormConfig>();
-  #http = inject(HttpClient);
+
 
   ngOnInit() {
     this.buildForm.emit(this.configDisplayOptions)
-  }
-  createFormComponent() {
-    this.#http.get<DynamicFormConfig>(`assets/company.form.json`).pipe(
-      tap((formConfig: DynamicFormConfig) => {
-        this.buildForm.emit(formConfig)
-      })
-    ).subscribe();
   }
 }
